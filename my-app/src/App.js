bffbca3eed9ee2b270988270a9fe91b266238246
Micah Anderson <micaherollyonelanderson@gmail.com>
2022-03-21 17:23:31 +0100

    Add json Data

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+import data from "./HackertNews.json";
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
+
+console.log(data);
